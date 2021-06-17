5040a78798fb22528cbcde170297d2e40c97b813
koliadjun <koliadjun@gmail.com>
2021-06-18 00:51:40 +0300

    task03 done

--- js/task-03.js
+++ js/task-03.js
@@ -2,18 +2,20 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
-    const delay = randomIntegerFromInterval(200, 500);
+const makeTransaction = (transaction) => {
+    return new Promise((resolve, reject) => {
+        const delay = randomIntegerFromInterval(200, 500);
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
 
-    setTimeout(() => {
-        const canProcess = Math.random() > 0.3;
+            if (canProcess) {
+                resolve(transaction.id, delay);
+            } else {
+                reject(transaction.id);
+            }
+        }, delay);
+    })
 
-        if (canProcess) {
-            onSuccess(transaction.id, delay);
-        } else {
-            onError(transaction.id);
-        }
-    }, delay);
 };
 
 const logSuccess = (id, time) => {
@@ -27,10 +29,10 @@ const logError = (id) => {
 /*
  * Працює так
  */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
  * Повинно працювати так
  */
