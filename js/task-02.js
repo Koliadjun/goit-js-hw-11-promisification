774d5b698f51e509255ef8db2faf85c01a9f1cf1
koliadjun <koliadjun@gmail.com>
2021-06-18 00:46:02 +0300

    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,12 @@ const users = [
     { name: "Lux", active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
+const toggleUserState = (allUsers, userName) => {
     const updatedUsers = allUsers.map((user) =>
         user.name === userName ? { ...user, active: !user.active } : user
     );
 
-    callback(updatedUsers);
+    return Promise.resolve(updatedUsers);
 };
 
 const logger = (updatedUsers) => console.table(updatedUsers);
@@ -19,8 +19,8 @@ const logger = (updatedUsers) => console.table(updatedUsers);
 /*
  * Зараз працює так
  */
-toggleUserState(users, "Mango", logger);
-toggleUserState(users, "Lux", logger);
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
 
 /*
  * Повинно працювати так
